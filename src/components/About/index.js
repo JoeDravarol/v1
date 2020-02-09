d2d60d6167b841da6070257c84653b234ce7090c
Joe <joedravarol@gmail.com>
2020-02-09 00:06:32 +0000

    Add About component

---
+++ src/components/About/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import './index.scss'
+
+const About = () => (
+  <section id="about" className="section about">
+    <h2 className="section__heading about__heading">About Me</h2>
+    <p className="section__text about__text">
+      Hello! I'm Joe, a Fullstack Web Developer based in Manchester who enjoys
+      building things that live on the internet. Like many other people, I like
+      spending my time increasing my knowledge of Web Development and tinkering
+      with new technologies.
+    </p>
+  </section>
+)
+
+export default About
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,13 @@ import React from 'react'
 import Layout from 'components/Layout'
 import SEO from 'components/Seo'
 import Home from 'components/Home'
+import About from 'components/About'
 
 const IndexPage = () => (
   <Layout>
     <SEO />
     <Home />
+    <About />
   </Layout>
 )
 
